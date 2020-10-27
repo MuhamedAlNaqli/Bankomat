8100052324282df6fcf7e9ed53d5c9536c2f26d7
Adnan Senad Imamovic <adnan.senad.imamovic@outlook.com>
2020-10-27 21:51:50 +0100

    Dodata meni klasa

---
+++ ATM/Meni.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class Meni
+    {
+        private void PrikaziMeni()
+        {
+            Console.WriteLine("Ovo je meni");
+            Console.ReadLine();
+        }
+            
+
+    }
+}
