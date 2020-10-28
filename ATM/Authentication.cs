210e8538ab3c8d2706f79b36730c7887af1c478e
Adnan Senad Imamovic <adnan.senad.imamovic@outlook.com>
2020-10-28 19:17:02 +0100

    Pomagaj

---
+++ ATM/Authentication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{                          //Napraviti klasu Authentication koja ce biti zaduzena za autentikaciju korisnika
+                           //Unutar klase potrebno dodati funkciju IsPinValid koja prima string, a treba da vrati bool
+                           //Funkciju implementirati na nacin da provjeri da li je uneseni string == "0000", ako jeste vratiti true, ako nije vratiti false
+
+    class Authentication
+    {
+        string pin = "";
+        
+          void  IsPinValid ()
+        {
+            Console.WriteLine("Unesite vaš pin:");
+           
+           
+
+
+        }
+    }
+}
