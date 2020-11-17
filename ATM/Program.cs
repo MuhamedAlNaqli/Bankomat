b0dd388058378b075a446fc1e48845fbdc22e3ec
Muki94 <muhamed.alnaqli.aplab12@gmail.com>
2020-11-17 10:23:25 +0100

    dodati novi taskovi

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -13,11 +13,6 @@ namespace ATM
         //git add .
         //git commit -m "neka poruka"
         //git push origin main  
-
-        //zadaci
-        
-        //2.2 Ako pin nije validan ispisati korisniku da nije validan pin 
-        //3. Omoguciti korisniku da unosi pin sve dok je netacan
         
         static void Main(string[] args)
         {
