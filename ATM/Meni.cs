6e72cd59b9634a436c99ab6039068ec845d7a2be
Muki94 <muhamed.alnaqli.aplab12@gmail.com>
2020-11-17 10:21:44 +0100

    dodati novi taskovi

--- ATM/Meni.cs
+++ ATM/Meni.cs
@@ -8,12 +8,10 @@ namespace ATM
 {
     public class Meni
     {
-
         public void LoginMeni()
         {
             Console.WriteLine("Dobro dosli u ATM, molimo vas unesite vaš pin:");
         }
-            
         public void PrikaziMeni()
         {
             Console.WriteLine("Dobro došli u ATM bankomat");
--- ATM/Program.cs
+++ ATM/Program.cs
@@ -25,16 +25,34 @@ namespace ATM
             Authentication Authmanager = new Authentication();
             string Pin = "";
 
-
             Menimanager.LoginMeni();
+
+
+
+
+
+
+            //napraviti ponavljanje unosa pina, max 3 pogresna unosa od korisnika
 
             Pin = Console.ReadLine();
 
             bool IsPinValid = Authmanager.IsPinValid(Pin);
 
+            //napraviti ponavljanje unosa pina, max 3 pogresna unosa od korisnika
+
+
+
+
+
+
             if (IsPinValid)
             {
+                //napraviti ponavljanje menija sve dok korisnik ne odabere izlaz
                 Menimanager.PrikaziMeni();
+                //napraviti ponavljanje menija sve dok korisnik ne odabere izlaz
+
+                //izmjeniti funkciju PrikaziMeni da vrati korisnikov odabir sta zeli sa menija i spremiti u kontenjer
+                //napraviti novu klasu Operacija u kojoj ce biti smjestene sve stvari: Uplata, Isplata, Izlaz
             }
         }
     }
